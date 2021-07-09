b6ec0233c3e1d7f73a78c794b17ef297b86a9516
MirkoDC-Aesys <85115982+MirkoDC-Aesys@users.noreply.github.com>
2021-07-09 15:51:55 +0200

    Add dispatch onClick in Counter.tsx

--- src/features/Counter/Counter.tsx
+++ src/features/Counter/Counter.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import classes from './Counter.module.css'
 import Button from '../../components/Button'
+import { useAppDispatch, useAppSelector } from '../../app/hooks'
+import { decremented, incremented } from './counterSlice'
 
 const Counter = () => {
+    const count = useAppSelector(state => state.counter.value)
+    const dispatch = useAppDispatch()
+
     return (
         <div className={classes.container}>
             <div className={classes.counterBox}>
                 <h2>Counter</h2>
-                <div>
-                    <Button onClick={() => alert('ciao')}>-</Button>
-                    <span>Count</span>
-                    <button>+</button>
+                <div className={classes.countContainer}>
+                    <Button onClick={() => dispatch(incremented())}>-</Button>
+                    <span className={classes.count}>{count}</span>
+                    <Button onClick={() => dispatch(decremented())}>+</Button>
                 </div>
             </div>
         </div>

04bb67777f4c6e1b3f7819e35d0dce7ee43200cf
MirkoDC-Aesys <85115982+MirkoDC-Aesys@users.noreply.github.com>
2021-07-09 11:35:48 +0200

    Add App Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
 import React from 'react'
 import './App.css'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      App
+    <div className="App">
+      <Router>
+        <div className="content-top">
+          Navbar
+        </div>
+        <div className="content-bottom">
+          <div className="sidebar">
+            Sidebar
+          </div>
+          <div className="content">
+            <div>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem voluptates qui ipsam in hic id quibusdam beatae aliquid error, facere iusto similique sunt saepe, laboriosam, esse quaerat cum sed perferendis!
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem voluptates qui ipsam in hic id quibusdam beatae aliquid error, facere iusto similique sunt saepe, laboriosam, esse quaerat cum sed perferendis!
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem voluptates qui ipsam in hic id quibusdam beatae aliquid error, facere iusto similique sunt saepe, laboriosam, esse quaerat cum sed perferendis!
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem voluptates qui ipsam in hic id quibusdam beatae aliquid error, facere iusto similique sunt saepe, laboriosam, esse quaerat cum sed perferendis!
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem voluptates qui ipsam in hic id quibusdam beatae aliquid error, facere iusto similique sunt saepe, laboriosam, esse quaerat cum sed perferendis!
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem voluptates qui ipsam in hic id quibusdam beatae aliquid error, facere iusto similique sunt saepe, laboriosam, esse quaerat cum sed perferendis!
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem voluptates qui ipsam in hic id quibusdam beatae aliquid error, facere iusto similique sunt saepe, laboriosam, esse quaerat cum sed perferendis!
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem voluptates qui ipsam in hic id quibusdam beatae aliquid error, facere iusto similique sunt saepe, laboriosam, esse quaerat cum sed perferendis!
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem voluptates qui ipsam in hic id quibusdam beatae aliquid error, facere iusto similique sunt saepe, laboriosam, esse quaerat cum sed perferendis!
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem voluptates qui ipsam in hic id quibusdam beatae aliquid error, facere iusto similique sunt saepe, laboriosam, esse quaerat cum sed perferendis!
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem voluptates qui ipsam in hic id quibusdam beatae aliquid error, facere iusto similique sunt saepe, laboriosam, esse quaerat cum sed perferendis!
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem voluptates qui ipsam in hic id quibusdam beatae aliquid error, facere iusto similique sunt saepe, laboriosam, esse quaerat cum sed perferendis!
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem voluptates qui ipsam in hic id quibusdam beatae aliquid error, facere iusto similique sunt saepe, laboriosam, esse quaerat cum sed perferendis!
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem voluptates qui ipsam in hic id quibusdam beatae aliquid error, facere iusto similique sunt saepe, laboriosam, esse quaerat cum sed perferendis!
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem voluptates qui ipsam in hic id quibusdam beatae aliquid error, facere iusto similique sunt saepe, laboriosam, esse quaerat cum sed perferendis!
+            </div>
+          </div>
+        </div>
+      </Router>
     </div>
   );
 }

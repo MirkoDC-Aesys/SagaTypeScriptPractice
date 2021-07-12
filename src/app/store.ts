43852d057b1eff1cf6d43880a9b39523d7d2d98d
MirkoDC-Aesys <85115982+MirkoDC-Aesys@users.noreply.github.com>
2021-07-12 11:23:52 +0200

    Add Cat component

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
 import createSagaMiddleware from 'redux-saga'
 import counterReducer from '../features/Counter/counterSlice'
+import catReducer from '../features/Cat/catSlice'
 import catWatcher from '../sagas/handlers/cat'
+
 
 const sagaMiddleware = createSagaMiddleware()
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    cat: catReducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware)
 })
--- src/features/Cat/Cat.tsx
+++ src/features/Cat/Cat.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { useAppDispatch, useAppSelector } from '../../app/hooks'
+import Button from '../../components/Button'
 import classes from './Cat.module.css'
+import { getCat } from './catSlice'
 
 const Cat = () => {
+    const { value, status } = useAppSelector(state => state.cat)
+    const dispatch = useAppDispatch()
+
     return (
-        <div>
-            Cat
+        <div className={classes.container}>
+            <div className={classes.counterBox}>
+                <Button onClick={() => dispatch(getCat())}>Get Cat</Button>
+                <div className={classes.img}>
+                    <img src={value} alt="" />
+                </div>
+                {
+                    status === 'failed' ?
+                        <div>Error</div>
+                        :
+                        <div></div>
+                }
+            </div>
         </div>
     )
 }
